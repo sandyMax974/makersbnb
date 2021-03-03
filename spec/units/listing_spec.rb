a13783136921e57ebb3dbca6069130b82364027f
Sandy <pounoussamy.sandy@gmail.com>
2021-03-03 16:35:33 +0000

    added the id to each listing instance

--- spec/units/listing_spec.rb
+++ spec/units/listing_spec.rb
@@ -2,12 +2,14 @@ require 'listing'
 
 describe Listing do
   describe '.create' do
-    it 'should create a new listing' do
+    it 'should create a new listing with an id, title, description and creator_id' do
 
       listing = Listing.create('Sea Cottage', 'Lovely holiday vacation', 23)
-      expect(listing.title).to eq 'Sea Cottage'
-      expect(listing.description).to eq 'Lovely holiday vacation'
-      expect(listing.creator_id).to eq 23
+
+      expect(listing.first.id).to_not be nil
+      expect(listing.first.title).to eq 'Sea Cottage'
+      expect(listing.first.description).to eq 'Lovely holiday vacation'
+      expect(listing.first.creator_id).to eq '23'
     end
   end
 
@@ -20,6 +22,8 @@ describe Listing do
 
       expect(listings[0].title).to eq 'Ski lodge in South of France'
       expect(listings[1].title).to eq 'Holiday house'
+      expect(listings[0].description).to eq 'Perfect for families'
+      expect(listings[1].description).to eq 'Amazing location'
     end
   end
 end
